fde92bbe6a3b500fe3784e17968a4a4ad9f687fa
bjornth2004 <113980359+bjornth2004@users.noreply.github.com>
2023-10-12 13:54:29 +0200

    index

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -1,3 +1,5 @@
+import { Huurder } from '../domain/model/Huurder';
+
 type HuurderInput = {
     naam?: string;
     id?: number;
@@ -8,3 +10,25 @@ type HuurderInput = {
     postcode?: string;
     telefoon?: string;
 };
+
+type VerhuurderInput = {
+    naam?: string;
+    achternaam?: string;
+    login?: boolean;
+    pass?: number;
+    iban?: string;
+    tel?: number;
+    email?: string;
+};
+
+type KotInput = {
+    id?: number;
+    huurder?: Huurder; // Assuming you import Huurder at the top
+    verhuurder?: string;
+    actief?: boolean;
+    oppervlakte?: number;
+    locatie?: string;
+    verhuurprijs?: number;
+}; //--> ook shit inmorten
+
+export { KotInput, HuurderInput, VerhuurderInput };
