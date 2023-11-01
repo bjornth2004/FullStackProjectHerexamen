ad1a95fdd364eefdd8ea90524b86b4b0608317ac
VicMenten <113980406+VicMenten@users.noreply.github.com>
2023-11-01 12:24:20 +0100

    verhuurder swagger

--- back-end/controller/verhuurder.route.ts
+++ back-end/controller/verhuurder.route.ts
@@ -25,6 +25,39 @@ import verhuurderService from '../service/Verhuurder.service';
 
 const verhuurderRouter = express.Router();
 
+/**
+ * @swagger
+ * /verhuurders:
+ *   post:
+ *     summary: Create a new Verhuurder
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Verhuurder'
+ *     responses:
+ *       200:
+ *         description: Verhuurder created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Verhuurder'
+ *       400:
+ *         description: Error creating Verhuurder
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 errorMessage:
+ *                   type: string
+ */
+
 verhuurderRouter.post('/', (req: Request, res: Response) => {
     try {
         //Try catch
@@ -36,6 +69,35 @@ verhuurderRouter.post('/', (req: Request, res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /verhuurders:
+ *   get:
+ *     summary: Get a list of Verhuurders
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of Verhuurders retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Verhuurder'
+ *       400:
+ *         description: Error retrieving Verhuurders
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 errorMessage:
+ *                   type: string
+ */
+
 verhuurderRouter.get('/', (req: Request, res: Response) => {
     //GET REQUEST IMPLEMENTEREN
     try {
@@ -45,6 +107,40 @@ verhuurderRouter.get('/', (req: Request, res: Response) => {
         res.status(400).json({ status: 'error', errorMessage: error.message }); //Als het fout gaat
     }
 });
+
+/**
+ * @swagger
+ * /verhuurders/{id}:
+ *   get:
+ *     summary: Get a Verhuurder by ID
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the Verhuurder to retrieve
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: Verhuurder retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Verhuurder'
+ *       400:
+ *         description: Error retrieving Verhuurder
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 errorMessage:
+ *                   type: string
+ */
 
 verhuurderRouter.get('/:id', (req: Request, res: Response) => {
     //GET REQUEST IMPLEMENTEREN
