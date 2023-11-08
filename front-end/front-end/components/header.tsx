ec6fa01f2ff3d154582dc74b42b6de6d6863fcfa
VicMenten <113980406+VicMenten@users.noreply.github.com>
2023-11-08 11:53:33 +0100

    header component

---
+++ front-end/front-end/components/header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Header: React.FC= () => {
+    return (
+        <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
+            <a className="fs-2 d-flex justify-content-center mb-2 mb-lg-0 text-white-50 text-decoration-none"> 
+                {" "}
+                Kotmeester
+            </a>
+            <nav className="nav justify-content-center">
+                <Link href="/" className="nav-link px-4 fs-5 text-white">
+                    Home
+                </Link>
+                <Link href="/koten" className="nav-link px-4 fs-5 text-white">
+                    Beschikbaare koten
+                </Link>
+                <Link href="/" className="nav-link px-4 fs-5 text-white">
+                    Profiel
+                </Link>
+                <Link href="/" className="nav-link px-4 fs-5 text-white">
+                    Mijn koten
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
--- front-end/front-end/services/KotService.ts
+++ front-end/front-end/services/KotService.ts
@@ -4,11 +4,16 @@ const getAllKoten = () => {
         headers: {
             "Content-Type": "application/json",
         }
-    })
+    });
 };
 
 const getKotById = {id: string} => {
-
+    return fetch(process.env.NEXT_PUBLIC_API_URL + `/koten/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 };
 
 const KotService = {
